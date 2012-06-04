e8640c343deb7b53be6c67a49b5b4e43c52c0c8f
andrea longhi <andrea@spaghetticode.it>
2012-06-04 23:19:52 +0200

    refactoring
    * remove downloader class
    
    * add video class
    
    * code cleanup

--- lib/youtube-ripper.rb
+++ lib/youtube-ripper.rb
@@ -1,12 +1,11 @@
-require "nokogiri"
-require "uri"
-require "cgi"
-require "open-uri"
-require "ostruct"
+require 'uri'
+require 'cgi'
+require 'open-uri'
+require 'nokogiri'
 
-require "youtube-ripper/version"
-require "youtube-ripper/ripper"
-require "youtube-ripper/downloader"
+require_relative 'youtube-ripper/version'
+require_relative 'youtube-ripper/video'
+require_relative 'youtube-ripper/ripper'
 
 module Youtube
 end
--- lib/youtube-ripper/version.rb
+++ lib/youtube-ripper/version.rb
@@ -1,5 +1,5 @@
 module Youtube
   class Ripper
-    VERSION = "0.0.1"
+    VERSION = '0.0.1'
   end
 end
